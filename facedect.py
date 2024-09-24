821fb7be5c7b1e3d93bbb5c4ae6ed4a79502dce7
Dave Persaud <davep218@outlook.com>
2024-09-24 09:34:19 -0400

    fixed the possibly working pauser?? idk i cant test it BECAUSE I HAVE NO DATA

--- facedect.py
+++ facedect.py
@@ -1,7 +1,7 @@
 
 import cv2
 import pickle
-
+import json
 video = cv2.VideoCapture(0)
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
@@ -19,8 +19,8 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     face = cascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5)
- 
-
+    count = 0
+    last =""
     for x,y,w,h in face:
         face_save = gray[y:y+h, x:x+w]
         
@@ -28,11 +28,18 @@ while True:
         ID, conf = recognise.predict(face_save)
        
         if conf >= 20 and conf <= 115:
+            if count > 30:
+                transfer = {"name":ID}
+                print(json.dumps(transfer))
+                count = 0
             print(ID)
+            if last == ID:
+                count = count + 1
             print(labels[ID])
             cv2.putText(frame,labels[ID],(x-10,y-10),cv2.FONT_HERSHEY_COMPLEX ,1, (18,5,255), 2, cv2.LINE_AA )
+            last = ID
         frame = cv2.rectangle(frame, (x,y), (x+w,y+h),(0,255,255),4)
-
+        
     cv2.imshow("Video",frame)
     key = cv2.waitKey(1)
     if(key == ord('q')):
